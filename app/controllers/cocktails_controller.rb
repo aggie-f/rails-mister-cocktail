ea2b56c059a0559101bcb427d7d1d89a34640d13
Agata Filus <agatafilus@gmail.com>
2021-03-13 18:15:47 +0000

    delete method added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -31,6 +31,12 @@ class CocktailsController < ApplicationController
     redirect_to cocktail_path(@cocktail.id)
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.delete
+    redirect_to root_path
+  end
+
   private
 
   def cocktail_params
