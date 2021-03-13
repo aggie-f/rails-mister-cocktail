c2bd501cddb6e3acb390f47c59091729d33a406d
Agata Filus <agatafilus@gmail.com>
2021-03-13 18:01:52 +0000

    edit controller and view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,6 +20,16 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail.id)
+  end
 
   private
 
